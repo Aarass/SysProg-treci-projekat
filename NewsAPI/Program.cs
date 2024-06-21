e80d31f2903475f3e3ff04c25041b4b5b5ffc9ad
Aarass <prokopovic75@gmail.com>
2024-06-21 21:16:44 +0200

    batman

---
+++ NewsAPI/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Reactive.Linq;
+
+namespace NewsAPI
+{
+    class Program
+    {
+        static void Main()
+        {
+            var source = Observable.Interval(TimeSpan.FromSeconds(1));
+
+            using (source.Subscribe(x => Console.WriteLine($"Received: {x}")))
+            {
+                Console.WriteLine("Press any key to exit...");
+                Console.ReadKey();
+            }
+        }
+    }
+
+}
